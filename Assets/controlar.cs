3a9cde1db9339fe66dafe95fe878c749551a4c66
Luis Granada <luisgranada1213@gmail.com>
2019-09-12 13:22:17 -0500

    Pantalla de intro y carro en movimiento

---
+++ Assets/controlar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlar : MonoBehaviour
+{
+    public void cambiarEscena(string nombre)
+    {
+        Application.LoadLevel(nombre);
+    }
+
+    public void cerrarJuego()
+    {
+        Application.Quit();
+    }
+}
